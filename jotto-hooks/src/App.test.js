25bb4e03095ccfd0176ee54d04d7d6fe2d78d5c2
Amrut Patil <apatil@emmes.com>
2019-12-10 17:23:59 -0500

    Basic app component

--- jotto-hooks/src/App.test.js
+++ jotto-hooks/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {shallow} from 'enzyme';
+import {findByTestAttr} from "../test/testUtils";
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/**
+ * Setup function for app component
+ * returns {ShallowWrapper}
+ */
+const setup = () => {
+    return shallow(<App />);
+}
+
+test('App renders without error', () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, 'component-app');
+    expect(component.length).toBe(1);
 });
+
+
